a2975f19b72d77c10577724c86758a9bcdd9aea3
Vo Nhat Tan <126853176+NhatTan1232@users.noreply.github.com>
2024-06-06 21:02:56 +0700

    Update exercise_1.sh

--- exercise_1.sh
+++ exercise_1.sh
@@ -21,10 +21,7 @@ echo "stop" >> words.txt
 touch reverse_words.txt
 
 # Read though each line in words.txt, reverse it and append to reverse_words.txt
-cat words.txt | while read line
-do
-	echo $line | rev >> reverse_words.txt
-done
+rev words.txt > reverse_words.txt
 
 # Read reverse_words.txt
 cat reverse_words.txt
